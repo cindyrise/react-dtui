00e8a0a6b52a6c3d84f94e6506bde7f9600078a3
zuoyi <zuoyi@dtstack.com>
2018-03-09 21:15:42 +0800

    测试是否能push

---
+++ src/components/ReactEcharts/index.js
@@ -0,0 +1,8 @@
+import PieChart from './piechart'
+import StackBar from './stackbar'
+import LineChart from './linechart'
+import ScatterChart from './scatterchart'
+import Map from './map'
+export {
+        PieChart,StackBar,LineChart,ScatterChart,Map
+}
