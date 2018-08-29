8fab77af8bdd113a7c88d50b0d58b2ef683f0126
yangyi <yhxyangyi@163.com>
2018-08-29 13:59:32 +0800

    rollup--update

--- tempbuild/libs/components/button/index.js
+++
@@ -1,6 +0,0 @@
-
-import Button from './button';
-import ButtonArea from './buttonArea';
-import ButtonPreview from './buttonPreview';
-import './style.scss';
-export { Button, ButtonArea, ButtonPreview };
--- tempbuild/libs/index.js
+++
@@ -1,9 +0,0 @@
-import version from './version';
-import { Button, ButtonArea, ButtonPreview } from './components/button/index';
-import './style/dtui.scss';
-export default {
-    Button: Button,
-    ButtonArea: ButtonArea,
-    ButtonPreview: ButtonPreview,
-    version: version
-};
